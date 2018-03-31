ecc35173c59f4721f7fa16273e042b82b3ad9172
Audrey McNay <amcnay@utexas.edu>
2018-03-31 18:47:12 -0500

    cuter lmao

--- mainPage/tiles.component.js
+++ mainPage/tiles.component.js
@@ -1,4 +1,4 @@
-angular.module('tiles', ['ngMaterial', 'ngMessages', 'material.svgAssetsCache'])
+angular.module('tiles', ['ngMaterial', 'ngMessages', 'material.svgAssetsCache']);
 
 angular.
     module('tiles').
@@ -7,11 +7,91 @@ angular.
         controller: function TilesCtrl($scope) {
 
             $scope.imagePath = 'img/water0.png';
+            $scope.eatingTrackerPictures = 'img/eat/5day_0.png';
             $scope.timeOfLastDrink = waterPicCounter;
             $scope.timerRunning = true;
+            $scope.waterHandlerEnabled = true;
+            $scope.standUpHandlerEnabled = true;
+	        $scope.fiveMeals = true;
             
-
+            var waterTimer = setInterval(drinkWaterHandler, 1000); //300000
+            var standUpTimer = setInterval(standUpHandler, 1000);   //600000
             var waterPicCounter = 0;
+            var eatPicCounter = 0;
+
+	        function drinkWaterHandler() {
+	           if ($scope.waterHandlerEnabled) {
+	               console.log("DRINK WATER"); //  replace with call to backend 
+	                }
+	            }
+	
+	            function standUpHandler() {
+	                if ($scope.standUpHandlerEnabled) {
+	                    console.log("stand up"); //  replace with call to backend 
+	                }
+	            }
+
+	            $scope.addToEatingPicture= function (){
+	                eatPicCounter++;
+	                $scope.changeEatingPicture();
+	                console.log("hey");
+	            }
+	            $scope.changeEatingPicture = function () {
+	
+	                console.log("changePictures");
+	                if ($scope.fiveMeals) {
+	                    switch (eatPicCounter) {
+	                        case 0:
+	                            $scope.eatingTrackerPictures = 'img/eat/5day_0.png';
+	                            break;
+	
+	                        case 1:
+	                            $scope.eatingTrackerPictures = 'img/eat/5day_1.png';
+	                            break;
+	
+	                        case 2:
+	                            $scope.eatingTrackerPictures = 'img/eat/5day_2.png';
+	                            break;
+	                        case 3:
+	                            $scope.eatingTrackerPictures = 'img/eat/5day_3.png';
+	                            break;
+	                        case 4:
+	                            $scope.eatingTrackerPictures = 'img/eat/5day_4.png';
+	                            break;
+	
+	                        case 5:
+	                            $scope.eatingTrackerPictures = 'img/eat/5day_5.png';
+	                            break;
+	                        default:
+	                            $scope.eatingTrackerPictures = 'img/eat/5day_5.png';
+	                            break;
+	                    }
+	                }
+	                else {
+
+	                    switch (eatPicCounter) {
+	                        case 0:
+	                            $scope.eatingTrackerPictures = 'img/eat/3day_0.png';
+	                            break;
+	
+	                        case 1:
+	                            $scope.eatingTrackerPictures = 'img/eat/3day_1.png';
+	                            break;
+	
+	                        case 2:
+	                            $scope.eatingTrackerPictures = 'img/eat/3day_2.png';
+	                            break;
+	                        case 3:
+                                $scope.eatingTrackerPictures = 'img/eat/3day_3.png';
+	                            break;	
+	                        default:
+	                            $scope.eatingTrackerPictures = 'img/eat/3day_3.png';
+	                            break;
+	                    }
+
+	                }
+	
+	            }
             $scope.changeWaterPicture = function () {
                 // $scope.timeOfLastDrink++;
                 waterPicCounter++;
@@ -68,7 +148,7 @@ angular.
 
 
     }).config(function ($mdThemingProvider) {
-        $mdThemingProvider.theme('dark-grey').backgroundPalette('grey').dark();
+        $mdThemingProvider.theme('dark-grey').backgroundPalette('blue').dark();
         $mdThemingProvider.theme('dark-orange').backgroundPalette('orange').dark();
         $mdThemingProvider.theme('dark-purple').backgroundPalette('deep-purple').dark();
         $mdThemingProvider.theme('dark-blue').backgroundPalette('blue').dark();
